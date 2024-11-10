7eed34e71b7b00f219ea3561b266461ea2584850
Bakam Shashidhar <114224818+Shashidhar03@users.noreply.github.com>
2024-11-11 02:04:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import ReactDOM from 'react-dom/client';
 import "../src/styles.css"
 
 import App from './components/App.jsx';
-import Test from './components/Test.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<Test />*/}
     <App />
   </React.StrictMode>
 );
